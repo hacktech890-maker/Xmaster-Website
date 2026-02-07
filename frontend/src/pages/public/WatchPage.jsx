b3492ee9586d65439c77c23f53463041f6063c87
Hack Tech <hacktech890@gmail.com>
2026-02-08 02:03:44 +0530

    Fix Watch.css import path

--- frontend/src/pages/public/WatchPage.jsx
+++ frontend/src/pages/public/WatchPage.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
-import '../styles/Watch.css';
+import '../../styles/Watch.css';
+
 
 function Watch() {
   const { id } = useParams();
